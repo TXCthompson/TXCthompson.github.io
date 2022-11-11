78ca107497f8a8faabe4df1c5421b451e9fb3936
onlytoxics <91626081+TXCthompson@users.noreply.github.com>
2022-11-11 23:18:13 +0200

    Update scripts.js

--- a/scripts/scripts.js
+++ a/scripts/scripts.js
@@ -74,21 +74,21 @@ function myChange() {
     } 
     else if (document.querySelector('.all').getAttribute('href') === 'permanent_marker') {
         let a = document.querySelector('.i1').value;
-        let b = "@import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');" + '\n' + '.s7 {font-family: "Permanent Marker", cursive;}' + '\n' + ' ' + '\n' + '<p class="s6">'+ a +'</p>';
+        let b = "@import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');" + '\n' + '.s7 {font-family: "Permanent Marker", cursive;}' + '\n' + ' ' + '\n' + '<p class="s7">'+ a +'</p>';
         document.querySelector('.o1').innerHTML = b;
         let c = document.querySelector(".s7");
         c.className = 's0';
     } 
     else if (document.querySelector('.all').getAttribute('href') === 'bebas_neue') {
         let a = document.querySelector('.i1').value;
-        let b = "@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');" + '\n' + '.s8 {font-family: "Bebas Neue", cursive;}' + '\n' + ' ' + '\n' + '<p class="s6">'+ a +'</p>';
+        let b = "@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');" + '\n' + '.s8 {font-family: "Bebas Neue", cursive;}' + '\n' + ' ' + '\n' + '<p class="s8">'+ a +'</p>';
         document.querySelector('.o1').innerHTML = b;
         let c = document.querySelector(".s8");
         c.className = 's0';
     } 
     else if (document.querySelector('.all').getAttribute('href') === 'henny_penny') {
         let a = document.querySelector('.i1').value;
-        let b = "@import url('https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap');" + '\n' + '.s9 {font-family: "Henny Penny", cursive;}' + '\n' + ' ' + '\n' + '<p class="s6">'+ a +'</p>';
+        let b = "@import url('https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap');" + '\n' + '.s9 {font-family: "Henny Penny", cursive;}' + '\n' + ' ' + '\n' + '<p class="s9">'+ a +'</p>';
         document.querySelector('.o1').innerHTML = b;
         let c = document.querySelector(".s9");
         c.className = 's0';
